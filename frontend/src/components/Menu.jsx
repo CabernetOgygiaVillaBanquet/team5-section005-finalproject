74004b806f0fb683bc6a506f53cea94031d24aad
Nicolas Cholin <nicolas.cholin@gmail.com>
2025-03-27 01:57:07 +0200

    Not necessary

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -14,7 +14,6 @@ const Menu = ({ onLogout }) => {
 
       {open && (
         <nav className="menu-content">
-          <a href="/">Home</a>
           <a href="/dashboard">Dashboard</a>
           <button className="logout-link" onClick={onLogout}>Logout</button>
         </nav>
