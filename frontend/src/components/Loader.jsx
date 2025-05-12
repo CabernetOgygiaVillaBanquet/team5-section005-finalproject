ce2d587f4235719899d213fa5eefc914edfb21b9
Nicolas Cholin <nicolas.cholin@gmail.com>
2025-05-12 16:17:28 +0300

    Loader change

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Loader.css';
 
-const Loader = () => (
-  <div className="loader-wrapper">
-    <div className="spinner">
-      <div className="ring"></div>
-      <div className="ring-glow"></div>
+const Loader = ({ message = "Loading..." }) => {
+  return (
+    <div className="hatom-loader" role="alert" aria-live="assertive">
+      <div className="loader-container">
+        <div className="ring"></div>
+        <div className="center-dot"></div>
+        {message && <p className="loading-text">{message}</p>}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Loader;
